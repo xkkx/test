29575b6f30eaf9b4575ff4ce3a9e2060ada9cc72
川瀬広大 <kodai@hagakure.local>
2018-05-12 23:26:35 +0900

    refs #3

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+package main;
+
+/**
+ * メイン処理クラス
+ */
 public class Main {
     public static void main(String[] args) {
+        // Hello world.を出力
         System.out.println("Hello world.");
     }
 }
